f628fa82caf82fb0a0e5e953cd76e413e3ccf84c
shinya koizumi <shin.koizumi@nttcsol.com>
2019-12-15 16:34:43 +0900

    search css modify

--- pages/search.vue
+++ pages/search.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="map">
-    ID検索<input type="text" v-model.number="searched_id">
-    <nuxt-link @click.native="search" to="/search_result">検索実行</nuxt-link>
-    <p>1,2,3のどれか。</p>
-    タイトル検索<input type="text" v-model="searched_title">
-    <nuxt-link @click.native="search_title" to="/search_result">検索実行</nuxt-link>
-    <p>蒙古タンメン中本 渋谷店or目黒店or渋谷店のどれか。</p>
+  <div>
+    <h2>検索画面</h2>
+    <input class="search-box" type="text" v-model.number="searched_id">
+    <nuxt-link class="btn-square" @click.native="search" to="/search_result">id検索</nuxt-link><br />
+    <input class="search-box" type="text" v-model="searched_title">
+    <nuxt-link class="btn-square" @click.native="search_title" to="/search_result">title検索</nuxt-link><br />
+    
+    <nuxt-link to="/view_data">全件データ表示</nuxt-link><br />
+    <p>[memo]idは1,2,3のどれか。</p>
+    <p>[memo]titleは蒙古タンメン中本 渋谷店or目黒店or渋谷店のどれか。</p>
+
   </div>
   
   
@@ -17,7 +21,7 @@
   export default {
     data: function() {
       return {
-        searched_id: 0,
+        searched_id: '',
         searched_title: ''
       }
 
@@ -31,7 +35,7 @@
     methods: {
       search(event){
         const id = this.searched_id
-        alert(id);
+        // alert(id);
         this.$store.dispatch('view_maps/search_id', id)
       },
       search_title(event){
@@ -49,5 +53,33 @@
 
 
 <style>
+
+.search-box {
+  box-sizing: border-box;
+  border: 1px solid #999;
+  padding: 3px 5px;
+  border-radius: 5px;
+  height: 2.0em;
+  width: 245px;
+  overflow: hidden;
+
+}
+
+.btn-square {
+  display: inline-block;
+  padding: 3px 5px;
+  text-decoration: none;
+  background: #668ad8;/*ボタン色*/
+  color: #FFF;
+  border-bottom: solid 4px #627295;
+  border-radius: 3px;
+  width: 80px;
+}
+.btn-square:active {
+  /*ボタンを押したとき*/
+  -webkit-transform: translateY(4px);
+  transform: translateY(4px);/*下に動く*/
+  border-bottom: none;/*線を消す*/
+}
 
 </style>
